34cef7c473a069df0f6c11f29eb2a3335374e814
pasanweerawardana <pasanm16gmail.com>
2023-03-01 07:54:01 +0530

    finishing the startup animation

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -1,5 +1,13 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class AppInitializer extends Application {
 
@@ -8,7 +16,14 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        URL splashResource = this.getClass().getResource("/view/SplashScreenForm.fxml");
+        Parent splashContainer = FXMLLoader.load(splashResource);
+        Scene splashScene = new Scene(splashContainer);
+        splashScene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setScene(splashScene);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
     }
 }
--- src/main/java/controller/SplashScreenFormController.java
+++ src/main/java/controller/SplashScreenFormController.java
@@ -1,7 +1,62 @@
 package controller;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class SplashScreenFormController {
+
     public Label lblStatus;
+
+    public void initialize(){
+        Timeline timeline = new Timeline();
+        KeyFrame keyFrame1 = new KeyFrame(Duration.millis(500), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                lblStatus.setText("Initializing the UI..!");
+            }
+        });
+        KeyFrame keyFrame2 = new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                lblStatus.setText("Loading Plugins..!");
+            }
+        });
+        KeyFrame keyFrame3 = new KeyFrame(Duration.millis(2000), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                lblStatus.setText("Setup the UI Logic..!");
+            }
+        });
+        KeyFrame keyFrame4 = new KeyFrame(Duration.millis(3500), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    URL resource = this.getClass().getResource("/view/TextEditorForm.fxml");
+                    Parent container = FXMLLoader.load(resource);
+                    Scene editorScene = new Scene(container);
+                    Stage primaryStage = new Stage();
+                    primaryStage.setScene(editorScene);
+                    primaryStage.setTitle("Text Editor");
+                    primaryStage.show();
+                    primaryStage.centerOnScreen();
+                    lblStatus.getScene().getWindow().hide();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        timeline.getKeyFrames().addAll(keyFrame1, keyFrame2, keyFrame3, keyFrame4);
+        timeline.playFromStart();
+    }
 }
