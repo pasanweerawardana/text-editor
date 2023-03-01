d0b2b1e3d2d8d342bfd3d2738b19c051442901de
pasanweerawardana <pasanm16gmail.com>
2023-03-01 13:02:18 +0530

    adding save form

---
+++ src/main/java/controller/SaveFormController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class SaveFormController {
+    public void btnBrowsePath_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnOK_OnAction(ActionEvent actionEvent) {
+    }
+}
